c4337c8876d4da9cf713248621a7c8e29c44ef4b
Eduardo Turcios <eddieturcios@outlook.com>
2022-05-01 00:06:23 -0400

    bug fixed firebase not connecting

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,29 @@
+const webpack = require('webpack');
 module.exports = {
-  entry: [
-    './client/index.js'
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.REACT_APP_FIREBASE_API_KEY': JSON.stringify(
+        'AIzaSyD9sFO267ezcwsjm-pgiNje4gz_TAEYH2o'
+      ),
+      'process.env.REACT_APP_FIREBASE_AUTH_DOMAIN': JSON.stringify(
+        'dev-easy-go-list.firebaseapp.com'
+      ),
+      'process.env.REACT_APP_FIREBASE_PROJECT_ID':
+        JSON.stringify('dev-easy-go-list'),
+      'process.env.REACT_APP_FIREBASE_STORAGE_BUCKET': JSON.stringify(
+        'dev-easy-go-list.appspot.com'
+      ),
+      'process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID':
+        JSON.stringify('116950216902'),
+      'process.env.REACT_APP_FIREBASE_APP_ID': JSON.stringify(
+        '1:116950216902:web:8fe74c38a6aeb35b9127c'
+      ),
+    }),
   ],
+  entry: ['./client/index.js'],
   output: {
     path: __dirname,
-    filename: './public/bundle.js'
+    filename: './public/bundle.js',
   },
   devtool: 'source-map',
   module: {
@@ -14,11 +33,9 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
-          presets: [
-            '@babel/preset-react'
-          ]
-        }
-      }
-    ]
-  }
-}
+          presets: ['@babel/preset-react'],
+        },
+      },
+    ],
+  },
+};
