4885f30e4375af46b2ef8bc75c98468ac9fdf4bf
Eduardo Turcios <eddieturcios@outlook.com>
2022-05-01 17:32:50 -0400

    updated decoded value to be only user id

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,12 +1,14 @@
-const router = require('express').Router();
 const admin = require('./config/firebase-config');
 
 async function decodeToken(req, res, next) {
-  const token = req.headers.authorization.split(' ')[1];
+  const token = req.headers.authorization
+    ? req.headers.authorization.split(' ')[1]
+    : '';
   try {
     const decodeValue = await admin.auth().verifyIdToken(token);
     if (decodeValue) {
-      req.user = decodeValue;
+      console.log(decodeValue);
+      req.user = decodeValue.user_id;
       return next();
     }
     return res.json({ message: 'Unauthorize Request' });
@@ -14,4 +16,4 @@ async function decodeToken(req, res, next) {
     return res.json({ message: 'Internal Error' });
   }
 }
-module.exports = router;
+module.exports = decodeToken;
