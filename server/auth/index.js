8eee9938a90849b2c24848f0946aa7ea6131cc9b
Eduardo Turcios <eddieturcios@outlook.com>
2022-05-01 15:24:01 -0400

    updated firebase config backend

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,5 +1,17 @@
 const router = require('express').Router();
-const {
-  models: { User },
-} = require('../db');
+const admin = require('./config/firebase-config');
+
+async function decodeToken(req, res, next) {
+  const token = req.headers.authorization.split(' ')[1];
+  try {
+    const decodeValue = await admin.auth().verifyIdToken(token);
+    if (decodeValue) {
+      req.user = decodeValue;
+      return next();
+    }
+    return res.json({ message: 'Unauthorize Request' });
+  } catch (e) {
+    return res.json({ message: 'Internal Error' });
+  }
+}
 module.exports = router;
