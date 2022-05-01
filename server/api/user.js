c38d4daa2090ebece3dbe736e11a7e2846526282
Eduardo Turcios <eddieturcios@outlook.com>
2022-05-01 18:23:51 -0400

    updated user route to only exclude user id

--- server/api/user.js
+++ server/api/user.js
@@ -10,6 +10,7 @@ router.get('/', decodeToken, async (req, res, next) => {
   try {
     const [user, hasCreatedUser] = await User.findOrCreate({
       where: { user_id: req.user },
+      attributes: { exclude: ['user_id'] },
     });
     res.json(user);
   } catch (err) {
